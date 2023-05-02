d761ade763ca6aa1cdc537a9992e40159e3f319e
Bella-Cullen <sxk666@mail.ustc.edu.cn>
2023-05-02 22:29:31 +0800

    ysc_5_2_22_29

--- UstcMap/pages/index/index.js
+++ UstcMap/pages/index/index.js
@@ -1,5 +1,3 @@
-var QQMapWX = require('../../map.js');
-var qqmapsdk;
 var app = getApp();
 Page({
   //设置下拉刷新
@@ -12,135 +10,152 @@ Page({
   },
   //data
   data:{
-    buildData:app.globalData.map,
+    buildData:app.globalData.map[0].data_main,
     hidden:true,
-    // 设置markers
+    search_hidden:true,
     markers:[],
-    //输入的查询地址
     inputvalue:'',
-    //起始地址
+    input_txt:'',
+    search_result:[],
     startPoint:null,
     endPoint:null,
     currentdatabase:null,
     place_img_src:[""],
     modalname:"",
+    center_lat:31.838293,
+    center_long:117.255652,
   },
   //获取输入的查询地址
+  //TODO:模糊搜索算法改进:严格还是宽松?条目数量限制?
   inputplace:function(e){
+    var value=e.detail.value;
+    var flag1=true;
+    if(value=="")flag1=false;
+    var arr1=[];
+    var arr2=[];
+    if(value.length==1){
+      arr1.push(value[0]);
+      arr2.push(value[0]);
+    }
+    else{
+      for(var i=0;i<value.length;i++){
+        if(i<value.length/2)arr1.push(value[i]);
+        else arr2.push(value[i]);
+      }
+    }
+    var text1='.*'+arr1.join('.*')+'.*';
+    var text2='.*'+arr2.join('.*')+'.*';
+    var re1=new RegExp(text1);
+    var re2=new RegExp(text2);
+    var place_lst=this.data.buildData;
+    var len=place_lst.length;
+    var res=[];
+    for(var i=0;i<len;i++){
+      if(flag1&&(re1.test(place_lst[i].name)||re2.test(place_lst[i].name)))res.push(place_lst[i]);
+    }
     this.setData({
-      inputvalue:e.detail.value
+      inputvalue:value,
+      search_result:res,
+      search_hidden:false,
+    })
+    if(res.length==0){
+      this.setData({
+        search_hidden:true,
+      })
+    }
+  },
+  getInputValue:function(e){
+    this.setData({
+      search_hidden:true,
+      inputvalue:e.currentTarget.dataset.postname,
+      input_txt:e.currentTarget.dataset.postname,
+    })
+  },
+  click_input:function(e){
+    if(this.data.search_result.length){
+      this.setData({
+        search_hidden:false,
+      })
+    }
+  },
+  map_click:function(e){
+    this.setData({
+      search_hidden:true,
     })
   },
   //搜索
-  //TODO:搞下拉框模糊搜索?
-  //TODO:将中心点移至第一个markers?
   nearby_search:function(){
     //text是输入的地址
     var text = this.data.inputvalue;
-
     //待匹配的地址数组
-    var place_lst=this.data.buildData[0].data;
+    var place_lst=this.data.buildData;
     var len=place_lst.length;
-
     //返回的匹配完成的地址数组
-    var res={
-      "data":[],
-    };
-
-    //匹配,模糊搜索
-    var re=new RegExp(text);
+    var res=[];
+    //匹配,精确搜索
     for(var i=0;i<len;i++){
-      if(re.test(place_lst[i].name)){
-        res.data.push(place_lst[i]);
+      if(text==place_lst[i].name){
+        res.push(place_lst[i]);
+        break;
       }
     }
-
     //弹出框
-    var texttitle = '共找到'+res.data.length+'个地点';
+    var texttitle = res.length?"搜索成功":"搜索失败";
     wx.showToast({
       title: texttitle,
       icon: 'success',
       duration: 2000
     })
-
     //更新markers标记点
-    var number = this.data.markers.length;
-    let markers = this.data.markers;
-    markers.splice(1,number-1)
-    this.setData({
-      markers:markers
-    })
-    for (var i = 0; i < res.data.length; i++) {
-      let lat = res.data[i].latitude;
-      let lon = res.data[i].longtitude;
-      let name = res.data[i].name;
-      var index = "markers["+(i+1)+"]";
+    if(res.length) {
+      let lat = res[0].latitude;
+      let lon = res[0].longtitude;
+      let name = res[0].name;
+      var mark0=this.data.markers[0];
+      var mark1={
+        id:1,
+        latitude: lat,
+        longitude: lon,
+        iconPath: "../../images/标记.png",
+        width: 25,
+        height: 25,
+        label: {
+          content: name,
+          color: '#FFFFFF',
+          bgColor:'#6495ED',
+          fontSize: 13,
+          anchorX:14,
+          anchorY:-24,
+          borderRadius: 5,
+          borderWidth: 1,
+          borderColor: '#6495ED',
+          padding: 2,
+        }
+      }
       this.setData({
-        [index]:{
-          id:i+1,
-          latitude: lat,
-          longitude: lon,
-          iconPath: "../../images/标记.png",
-          width: 25,
-          height: 25,
-          label: {
-            content: name,
-            color: '#FFFFFF',
-            bgColor:'#6495ED',
-            fontSize: 13,
-            anchorX:14,
-            anchorY:-24,
-            borderRadius: 5,
-            borderWidth: 1,
-            borderColor: '#6495ED',
-            padding: 2,
-            //display: 'ALWAYS'
-          }
-        },
-        currentdatabase:res.data
+        markers:[mark0,mark1],
+        currentdatabase:res[0],
+        center_long:lon,
+        center_lat:lat,
+      })
+    }
+    else{
+      var mark0=this.data.markers[0];
+      this.setData({
+        markers:[mark0],
       })
     }
   },
-  //获取自己当前的位置
-  getmyPlace:function(){
-    var that=this;
-    wx.getLocation({
-      success: function(res) {
-        console.log(res);
-        that.setData({
-          markers:[
-            {
-              latitude:res.latitude,
-              longitude:res.longitude,
-              iconPath:"../../images/mapcenter.png",
-              width:25,
-              height:25,
-              callout:{
-                content:"当前位置",
-                color:'#0000ff',
-                fontSize:13,
-                borderRadius:5,
-                borderWidth:1,
-                borderColor:'#0000ff',
-                padding:2,
-                display:'ALWAYS'
-              }
-            }
-          ]
-        })
-      },
-    })
-  },
   //生命周期函数--监听页面加载
   onLoad: function (options) {
-    wx.stopPullDownRefresh() //刷新完成后停止下拉刷新动效
-        // 实例化API核心类
-        qqmapsdk = new QQMapWX({
-          key: '6IFBZ-ZYQ3I-UFHGJ-UVPZH-3HI2V-EQBBY'
-      });
+    wx.stopPullDownRefresh() //刷新完成后停止下拉刷新动效       
       var that = this;
+      this.setData({
+        search_hidden:true,
+      })
       //判断所在位置是否在校区内
       wx.getLocation({
+        isHighAccuracy:true,
 		    type:'gcj02',
         success:function(res)
         {
@@ -169,7 +184,9 @@ Page({
                     display:'ALWAYS'
                   }
                 }
-              ]
+              ],
+              center_lat:nowlatitude,
+              center_long:nowlongitude,
             })
           }else{
             wx.showModal({
@@ -199,7 +216,9 @@ Page({
                           display:'BYCLICK'
                         }
                       }
-                    ]
+                    ],
+                    center_lat:31.838293,
+                    center_long:117.255652,
                   })
                 }else if(res.cancel)
                 {
@@ -223,7 +242,9 @@ Page({
                           display:'ALWAYS'
                         }
                       }
-                    ]
+                    ],
+                    center_lat:nowlatitude,
+                    center_long:nowlongitude,
                   })
                 }
               }
@@ -248,42 +269,24 @@ Page({
       }
     })
   },
-
   //点击地点进行路径规划
   onPointTap: function(e) {
-    var that = this;
-    var lat = ''; // 获取点击的markers经纬度
-    var lon = ''; // 获取点击的markers经纬度
-    var name = ''; // 获取点击的markers名称
-    var markerId = e.detail.markerId;// 获取点击的markers  id
-    var markersda = this.data.markers;
-    var currentdatabase = this.data.currentdatabase;
-    //定位所点击的坐标点
-    for (var item of markersda){
-      if (item.id === markerId) {
-        lat = item.latitude;
-        lon = item.longitude;
-        name = item.label.content;
-        break;
-      }
-    }
-    //初始化起点为西大门门口
     var startPoint = JSON.stringify({
-      'name': markersda[0].callout.content,
-      'latitude': markersda[0].latitude,
-      'longitude': markersda[0].longitude
+      'name':this.data.markers[0].callout.content,
+      'latitude':this.data.markers[0].latitude,
+      'longitude':this.data.markers[0].longitude
     });
-    var endPoint = JSON.stringify({  //终点
-        'name': name,
-        'latitude': lat,
-        'longitude': lon
+    var endPoint = JSON.stringify({
+        'name': this.data.markers[1].label.content,
+        'latitude': this.data.markers[1].latitude,
+        'longitude': this.data.markers[1].longitude,
     });
-    that.setData({
+    this.setData({
       hidden:false,
-      modalname:currentdatabase[markerId-1].name,
+      modalname:this.data.currentdatabase.name,
       startPoint:startPoint,
       endPoint:endPoint,
-      place_img_src:currentdatabase[markerId-1].img
+      place_img_src:this.data.currentdatabase.img
     })
   },
   modalcancel:function(e)
@@ -317,5 +320,4 @@ Page({
       })
     })
   },
-  
 })
